65401fc8aa9b3b9882e557ff6929d3b23d81cc39
Cerberus <51792843+Cerberus22@users.noreply.github.com>
2021-05-27 16:48:54 +0200

    Create Guessing Game.py

---
+++ Challenges/1. Guessing Game/Guessing Game.py
@@ -0,0 +1,36 @@
+import random
+import time
+global lower, upper, difficulty, right_answer, attempts_amount, attempts
+def ask_bounds():
+    global lower, upper, difficulty
+    lower = int(input("Lower bound? "))
+    upper = int(input("Upper bound? "))
+    difficulty = int(input("Difficulty? "))
+ask_bounds()
+if lower >= upper or difficulty > upper - lower:
+    print("Incorrect bounds or difficulty, try again.")
+    ask_bounds()
+right_answer = random.randint(lower, upper)
+
+attempts = 0
+attempts_amount = int((upper - lower)/difficulty)
+print(f"You have {attempts_amount} attempts!")
+
+def guess():
+    global right_answer, attempts_amount, attempts
+    attempts += 1
+    attempts_left = attempts_amount - attempts
+    guess = int(input("Guess? "))
+    if guess == right_answer:
+        print("That was the right number, congrats!")
+        print(f"Guessed the number in {attempts} attempts.")
+        time.sleep(5)
+        quit()
+    elif attempts_amount != attempts:
+        print(f"Wrong! {attempts_left} attempts left.")
+    else:
+        print(f"Out of attempts, the answer was {right_answer}. You had {attempts_amount} attempts.")
+        time.sleep(5)
+        quit()
+while True:
+    guess()
